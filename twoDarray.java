78aaa2d722f70199d00105674747738e9af9297c
harshkumar132 <harshrohra573@gmail.com>
2025-07-12 19:13:46 +0500

    today i update twodarray file

---
+++ twoDarray.java
@@ -0,0 +1,13 @@
+
+
+public class twoDarray {
+    public static void main (String args[]){
+        int two[][]={{1,2,3},{4,5,6}};
+        for (int i=0;i<2;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(two[i][j]+" ");
+            }
+        }
+    }
+    
+}
