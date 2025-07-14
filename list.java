a1198338cf4da8df2367b0661977ef08c63e792f
harshkumar132 <harshrohra573@gmail.com>
2025-07-14 17:09:29 +0500

    In array list exercise   i did  //Create an ArrayList of integers, add 5 numbers, and print them using a loop

--- list.java
+++ list.java
@@ -1,5 +1,38 @@
- public class list  {
-    public static void main(String args[]){
-        System.out.println("hello world");
+import java.util.ArrayList;
+import java.util.Collections;
+public class list{
+    public static void main(String[] args) {
+        ArrayList<Integer> line = new ArrayList<>();
+        //add element in array list
+        line.add(1);
+        line.add(2);
+        line.add(3);
+
+        System.out.println(line);
+
+        // get element
+        int element = line.get(0);
+        System.out.println(element);
+
+        // add element between array list
+        line.add(0,7);
+        System.out.println(line);
+
+
+        //to change elemnt from array list 
+        line.set(2,10);
+        System.out.println(line);
+
+        line.remove(1);
+        System.out.println(line);
+
+
+        //size of array list
+        System.out.println(line.size());
+        
+        // sorting
+         Collections.sort(line);
+        System.out.println(line);
+        
     }
 }
